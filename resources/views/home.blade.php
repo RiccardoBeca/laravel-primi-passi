3c8cd09312349c9d1e63f76e68f5471ded1fe67f
RiccardoBeca <riccardobeca@gmail.com>
2022-06-22 14:34:23 +0100

    consegna senza bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,33 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="CSS/style.css">
   <title>Laravel-primi-passi</title>
 </head>
 <body>
   <h1>HELLO WORLD!</h1>
+  <h2>Hello {{$user}}!!</h2>
+
+  <h3>Here's a list of your favourite colors:</h3>
+
+  <ul>
+
+    @foreach ($favourite_colors as $color)
+
+      @if($loop->first)
+        <li class="orange">{{ $color }}</li>
+      @elseif ($loop->last)
+        <li class="purple"> {{ $color }}</li>
+      @else
+        <li>{{$color}}</li>
+   
+      @endif
+    @endforeach
+
+
+  </ul>
+
+
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $data = [
+
+        'user' => 'Moccariol Bocattins',
+        'favourite_colors' => ['orange','black','purple'],
+        'occupation' => 'scammer',
+
+    ];
+    return view('home', $data);
 });
